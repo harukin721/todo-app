e6f8b29b5855d105526a2a5b8fd0e7d7d71a2963
HARUKI UEJIMA <h.ueji.43@gmail.com>
2025-11-08 22:05:16 +0900

    feat: add log setting

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"github/harukin721/todo-app/utils"
 	"log"
 
 	"gopkg.in/ini.v1"
@@ -17,6 +18,7 @@ var Config ConfigList
 
 func init() {
 	LoadConfig()
+	utils.LoggingSettings(Config.LogFile)
 }
 
 func LoadConfig() {
