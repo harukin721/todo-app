c1b0d48f95bc667bc628b0fafe00b6ab1f6c177b
HARUKI UEJIMA <h.ueji.43@gmail.com>
2025-11-08 23:57:58 +0900

    feat: create todo db

--- main.go
+++ main.go
@@ -6,36 +6,5 @@ import (
 )
 
 func main() {
-	// fmt.Println(config.Config.Port)
-	// fmt.Println(config.Config.SQLDriver)
-	// fmt.Println(config.Config.DbName)
-	// fmt.Println(config.Config.LogFile)
-
-	// log.Println("test")
-
 	fmt.Println(models.Db)
-
-	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@example.com"
-	// u.Password = "password"
-	// fmt.Println(u)
-
-	// u.CreateUser()
-
-	fmt.Println("----- Get User -----")
-	u, _ := models.GetUser(1)
-	fmt.Println(u)
-
-	fmt.Println("----- Update User -----")
-	u.Name = "test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
-
-	fmt.Println("----- Delete User -----")
-	u.DeleteUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
 }
