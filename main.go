5997f89bfff7c752e1aaa3706276b1cb71bec3a5
HARUKI UEJIMA <h.ueji.43@gmail.com>
2025-11-09 00:07:08 +0900

    feat: create todo

--- main.go
+++ main.go
@@ -7,4 +7,15 @@ import (
 
 func main() {
 	fmt.Println(models.Db)
+
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.Password = "password"
+	fmt.Println(u)
+
+	u.CreateUser()
+
+	user, _ := models.GetUser(2)
+	user.CreateTodo("First Todo")
 }
